c71f70cc9d3db548abce8441b42d9e5ef07493a7
Tatiana Gepard <gepardtatiana@gmail.com>
2023-04-24 16:34:40 +0300

    refactor: add BEM notation for class names according to rule 2.1 in index.html

--- app.js
+++ app.js
@@ -46,8 +46,11 @@ var createNewTaskElement=function(taskString){
   editButton.innerText='Edit'; //innerText encodes special characters, HTML does not.
   editButton.classList.add('button', 'edit');
 
-  deleteButton.className="delete";
-  deleteButtonImg.src="./remove.svg";
+  deleteButton.classList.add('button', 'delete');
+
+  deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.classList.add('button__img');
+
   deleteButton.appendChild(deleteButtonImg);
 
 
@@ -63,7 +66,7 @@ var createNewTaskElement=function(taskString){
 
 
 var addTask=function(){
-  console.log("Add Task...");
+  console.log('Add Task...');
   //Create a new list item with the text from the #new-task:
   if (!taskInput.value) return;
   var listItem=createNewTaskElement(taskInput.value);
@@ -72,42 +75,42 @@ var addTask=function(){
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 
-  taskInput.value="";
+  taskInput.value='';
 }
 
 //Edit an existing task.
 
 var editTask=function(){
-  console.log("Edit Task...");
+  console.log('Edit Task...');
   console.log("Change 'edit' to 'save'");
 
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector("input[type=text]");
-  var label=listItem.querySelector("label");
-  var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("editMode");
+  var editInput=listItem.querySelector('.row__input');
+  var label=listItem.querySelector('.row__label');
+  var editBtn=listItem.querySelector('.edit');
+  var containsClass=listItem.classList.contains('editMode');
   //If class of the parent is .editmode
   if(containsClass){
 
       //switch to .editmode
       //label becomes the inputs value.
       label.innerText=editInput.value;
-      editBtn.innerText="Edit";
+      editBtn.innerText='Edit';
     }else{
       editInput.value=label.innerText;
-      editBtn.innerText="Save";
+      editBtn.innerText='Save';
     }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle('editMode');
 };
 
 
 //Delete task.
 var deleteTask=function(){
-  console.log("Delete Task...");
+  console.log('Delete Task...');
 
   var listItem=this.parentNode;
   var ul=listItem.parentNode;
@@ -119,7 +122,7 @@ var deleteTask=function(){
 
 //Mark task completed
 var taskCompleted=function(){
-  console.log("Complete Task...");
+  console.log('Complete Task...');
 
   //Append the task list item to the #completed-tasks
   var listItem=this.parentNode;
@@ -130,7 +133,7 @@ var taskCompleted=function(){
 
 
 var taskIncomplete=function(){
-  console.log("Incomplete Task...");
+  console.log('Incomplete Task...');
   //Mark task as incomplete.
   //When the checkbox is unchecked
   //Append the task list item to the #incompleteTasks.
@@ -142,7 +145,7 @@ var taskIncomplete=function(){
 
 
 var ajaxRequest=function(){
-  console.log("AJAX Request");
+  console.log('AJAX Request');
 }
 
 //The glue to hold it all together.
@@ -150,16 +153,16 @@ var ajaxRequest=function(){
 
 //Set the click handler to the addTask function.
 addButton.onclick=addTask;
-addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
+addButton.addEventListener('click',addTask);
+addButton.addEventListener('click',ajaxRequest);
 
 
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-  console.log("bind list item events");
+  console.log('bind list item events');
   //select ListItems children
-  var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var checkBox=taskListItem.querySelector('row__checkbox');
+  var editButton=taskListItem.querySelector('.edit');
+  var deleteButton=taskListItem.querySelector('.delete');
 
 
   //Bind editTask to edit button.
